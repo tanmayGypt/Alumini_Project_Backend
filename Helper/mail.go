73a8daac8e38528ada130c0d519a08bd4b34c8a9
mohitgusain8671 <mohitgusain8671@gmail.com>
2024-07-27 17:03:08 +0530

    .env updated with smtp variables

--- Helper/mail.go
+++ Helper/mail.go
@@ -1,33 +1,30 @@
 package helper
 
 import (
+	"os"
+	"strconv"
+
 	"gopkg.in/gomail.v2"
 )
-// func SendEmail(recipient string,) error {
-// 	m := gomail.NewMessage()
-// 	m.SetHeader("From", "mohit84cseb22@bpitindia.edu.in")
-// 	m.SetHeader("To", recipient)
-// 	m.SetHeader("Subject", "Password Reset")
-// 	m.SetBody("text/html", `<p>Click <a href="https://example.com/reset-password?token=your-reset-token">here</a> to reset your password.</p>`)
-
-// 	d := gomail.NewDialer("smtp.office365.com", 587, "mohit84cseb22@bpitindia.edu.in", "Abhishek@1998")
-
-// 	// Send the email
-// 	if err := d.DialAndSend(m); err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
 
 func SendEmail(recipient string, subject string, body string) error {
+	smtpServer := os.Getenv("SMTP_SERVER")
+	smtpPort := os.Getenv("SMTP_PORT")
+	emailUser := os.Getenv("EMAIL_USER")
+	emailPassword := os.Getenv("EMAIL_PASSWORD")
+
+	// Convert SMTP_PORT to int
+	smtpPortInt, err := strconv.Atoi(smtpPort)
+	if err != nil {
+		return err
+	}
 	m := gomail.NewMessage()
 
-	m.SetHeader("From", "mohit84cseb22@bpitindia.edu.in")
+	m.SetHeader("From", emailUser)
 	m.SetHeader("To", recipient)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body)
-	d := gomail.NewDialer("smtp.office365.com", 587, "mohit84cseb22@bpitindia.edu.in", "Abhishek@1998")
+	d := gomail.NewDialer(smtpServer, smtpPortInt, emailUser, emailPassword)
 
 	// Send the email
 	if err := d.DialAndSend(m); err != nil {
